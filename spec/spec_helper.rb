715706107578e34d0f0c3e57e57a8cfd35a6be40
Pablo Vicente <vicapa99@gmail.com>
2015-12-17 14:12:29 +0100

    Add RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.disable_monkey_patching!
+  config.warnings = true
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.profile_examples = 3
+  config.order = :random
+  Kernel.srand config.seed
+end
