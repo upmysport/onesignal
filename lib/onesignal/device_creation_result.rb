082e74cdbfd6c1def6619c7d241368798f1aeda0
Pablo Vicente <vicapa99@gmail.com>
2016-02-22 09:21:50 +0100

    DeviceCreationResult maps gateway response

--- lib/onesignal/device_creation_result.rb
+++ lib/onesignal/device_creation_result.rb
@@ -5,27 +5,32 @@ module Onesignal
   # The DeviceCreationResult class is responsible of exposing the results of a device creatino operation
   # @since 0.0.1
   class DeviceCreationResult
-    extend Forwardable
-
     # @return [String] Onesignal device identifier
     attr_reader :device_id
-    def_delegators :@result, :success?, :errors
+    # @return [Array] Array of error messages
+    attr_reader :errors
 
-    def initialize(result, gateway_response)
-      @result = result
-      @device_id = gateway_response.fetch(:id, '')
+    def initialize(status, response_body)
+      @success = (status == 200)
+      @device_id = response_body.fetch('id', '')
+      @errors = response_body.fetch('errors', [])
     end
 
     # Builds a DeviceCreationResult from a gateway response
     # @return [DeviceCreationResult]
     def self.from_device_creation(gateway_response)
-      DeviceCreationResult.new(Result.new(gateway_response), gateway_response)
+      DeviceCreationResult.new(gateway_response.status, gateway_response.body)
+    end
+
+    # @return [Boolean] Returns true when the operation was success
+    def success?
+      @success
     end
 
     def to_s
-      return @result.to_s unless success?
+      return "Errors: #{@errors.join(', ')}" unless success?
 
-      "#{@result}, device_id: #{device_id}"
+      "Success, device_id: #{device_id}"
     end
   end
 end
--- spec/device_creation_result_spec.rb
+++ spec/device_creation_result_spec.rb
@@ -5,15 +5,47 @@ module Onesignal
   RSpec.describe DeviceCreationResult do
     subject(:result) { described_class.from_device_creation(response) }
 
+    describe '.from_device_creation' do
+      let(:id) { '7c77ae44-b4eb-43cc-abfd-27cc4e4c23c7' }
+      let(:response) { double(status: 200, body: { 'id' => id }) }
+
+      it 'maps the response body values' do
+        expect(result.device_id).to eq(id)
+        expect(result.errors).to be_empty
+        expect(result.errors).to be_empty
+      end
+
+      it 'maps the status to success' do
+        expect(result).to be_success
+      end
+
+      context 'when response is not sucess' do
+        let(:errors) { ['one error'] }
+        let(:response) { double(status: 400, body: { 'errors' => errors }) }
+
+        it 'sets the device_id to empty' do
+          expect(result.device_id).to eq('')
+        end
+
+        it 'maps the errors' do
+          expect(result.errors).to eq(errors)
+        end
+
+        it 'maps the status to not success' do
+          expect(result).to_not be_success
+        end
+      end
+    end
+
     describe '#to_s' do
-      let(:response) { { id: '1234' } }
+      let(:response) { double(status: 200, body: { 'id' => '1234' }) }
 
       it 'returns a string with the result values' do
         expect(result.to_s).to eq('Success, device_id: 1234')
       end
 
       context 'when response has errors' do
-        let(:response) { { errors: ['one error'] } }
+        let(:response) { double(status: 400, body: { 'errors' => ['one error'] }) }
 
         it 'returns a string with the erros' do
           expect(result.to_s).to eq('Errors: one error')
