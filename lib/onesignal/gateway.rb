a5170c163cf15a53cd480580ca69b54e69043a3c
Pablo Vicente <vicapa99@gmail.com>
2016-04-15 21:12:05 +0200

    Extract class HTTPClient

--- lib/onesignal/gateway.rb
+++ lib/onesignal/gateway.rb
@@ -1,40 +1,26 @@
-require 'faraday'
-require 'faraday_middleware'
+require 'onesignal/http_client'
 
 module Onesignal
   # The Gateway class makes HTTP requests to the Onesignal REST API
   class Gateway
-    URL = 'https://onesignal.com'.freeze
     DEVICES_ENDPOINT = 'api/v1/players'.freeze
     NOTIFICATIONS_ENDPOINT = 'api/v1/notifications'.freeze
     STATUSES_WITHOUT_BODY = [204, 304].freeze
 
-    def initialize(app_id = Onesignal.app_id, log = Onesignal.log)
-      @http_client = create_http_client(log)
-      @app_id = app_id
+    def initialize(http_client = HTTPClient.new)
+      @http_client = http_client
     end
 
     # Makes a POST request to the players endpoint
     # @return [Hash]
     def create_device(params = {})
-      @http_client.post(DEVICES_ENDPOINT, params.merge(app_id: @app_id))
+      @http_client.post(DEVICES_ENDPOINT, params)
     end
 
     # Makes a POST request to the notifications endpoint
     # @return [Hash]
     def create_notification(params = {})
-      @http_client.post(NOTIFICATIONS_ENDPOINT, params.merge(app_id: @app_id))
-    end
-
-    private
-
-    def create_http_client(log)
-      Faraday.new(url: URL) do |faraday|
-        faraday.request :json
-        faraday.response :json, content_type: /\bjson$/
-        faraday.adapter Faraday.default_adapter
-        faraday.response :logger, log, bodies: true
-      end
+      @http_client.post(NOTIFICATIONS_ENDPOINT, params)
     end
   end
 end
---
+++ lib/onesignal/http_client.rb
@@ -0,0 +1,22 @@
+require 'faraday'
+require 'faraday_middleware'
+
+module Onesignal
+  class HTTPClient
+    URL = 'https://onesignal.com'.freeze
+
+    def initialize(log = Onesignal.log, app_id = Onesignal.app_id)
+      @app_id = app_id
+      @connection = Faraday.new(url: URL) do |connection|
+        connection.request :json
+        connection.response :json, content_type: /\bjson$/
+        connection.adapter Faraday.default_adapter
+        connection.response :logger, log, bodies: true
+      end
+    end
+
+    def post(endpoint, params)
+      @connection.post(endpoint, params.merge(app_id: @app_id))
+    end
+  end
+end
